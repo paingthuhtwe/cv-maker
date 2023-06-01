bedd27d940b45a3aeaebe71496c69d50c134694c
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-06-01 16:56:42 +0630

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="w-11/12 mx-auto">
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style>
@@ -11,7 +12,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
